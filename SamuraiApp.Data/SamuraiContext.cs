e2ff0599473ae067598b179f9dd64783f42f3e83
FazalFarruq <fazal.farruq@fiserv.com>
2021-09-06 10:50:41 -0400

    Add project files.

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=tcp:nsdfazaltest.database.windows.net,1433;Initial Catalog=SamuraiAppData;Persist Security Info=False;" +
+                "User ID=ffarruq;Password=Y@hoo89a!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
